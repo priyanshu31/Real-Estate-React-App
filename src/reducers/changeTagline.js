c897ae0a85e0f7ada9718199da60a64d4cd692b5
priyanshu31 <priyanshu@github.com>
2021-07-18 14:20:34 +0530

    Tagline Bug Fixed

--- src/reducers/changeTagline.js
+++ src/reducers/changeTagline.js
@@ -3,7 +3,7 @@ const intialTagline = "Buy your Dream House";
 const changeTagline = (state = intialTagline, action) => {
 
     switch (action.type) {
-        case "BUY": return state;
+        case "BUY": return intialTagline;
         case "RENT": return "Rent a Property";
         case "PLOT": return "Search upon the variaty of Plot we are having"
         case "COMMERCIAL": return "We have a lot of option for your property"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import changeTagline from './changeTagline'
-// import changeBackground from './changeBackground'
 
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
     changeTagline,
-    // changeBackground
 })
 
 export default rootReducer
